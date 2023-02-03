e6e955fb47443b4e49866b55dbc0838757533d49
VadymBoiko <boykovadymvas@gmail.com>
2023-02-03 10:30:02 +0200

    feat: add route in product list

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, FC } from 'react';
 import { getProducts } from '../../features/products/productSlice';
 import { useAppDispatch, useAppSelector } from '../../hooks/useTypedSelector';
 import { Product } from '../Product/Product';
 
 
+interface ProductListProps {
+  category: string;
+}
 
-
-export const ProductList = () => {
+export const ProductList: FC<ProductListProps> = ({ category }) => {
 
   const dispatch = useAppDispatch();
 
-  const { data, error, loading} = useAppSelector((state) => state)
+  const { data, error, loading } = useAppSelector((state) => state)
 
   useEffect(() => {
     dispatch(getProducts())
@@ -18,13 +20,19 @@ export const ProductList = () => {
 
   return (
     <div>
-         <div className="products">
-          {error && <p>oh no that is error</p>}
-          {loading && <p>loading...</p>}
-          {data && data.map(product => (
-            <Product product={product} key={product.id}/>
-          ))}
-        </div>
+      <div className="products">
+        {error && <p>oh no that is error</p>}
+        {loading && <p>loading...</p>}
+        {data && data.filter((product) => {
+          if (category === 'all') {
+            return data
+          } else {
+            return product.category === category
+          }
+        }).map(product => (
+          <Product product={product} key={product.id} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
 import { ProductList } from "../../components/ProductList/ProductList";
 
 
 export const Shop = () => {
+
+  const [category, setCategory] = useState<string>('all')
+
   return (
     <div className="container">
       <div className="shop-wrapper">
         <h1 className="title">Shop</h1>
         <Breadcrumbs />
         <ul className="categories">
-          <NavLink to='#' className="categories__link"> All</NavLink>
-          <NavLink to='#' className="categories__link">Topcoats</NavLink>
-          <NavLink to='#' className="categories__link">Sweatshirts</NavLink>
-          <NavLink to='#' className="categories__link">Cardigans</NavLink>
-          <NavLink to='#' className="categories__link">Hoodies</NavLink>
+          <li className={`categories__link ${category === 'all' && 'categories__link_active'}`} onClick={() => setCategory('all')}> All</li>
+          <li className={`categories__link ${category === 'topcoats' && 'categories__link_active'}`} onClick={() => setCategory('topcoats')}>Topcoats</li>
+          <li className={`categories__link ${category === 't-shirts' && 'categories__link_active'}`} onClick={() => setCategory('t-shirts')}>T-shirts</li>
+          <li className={`categories__link ${category === 'sweatshirts' && 'categories__link_active'}`} onClick={() => setCategory('sweatshirts')}>Sweatshirts</li>
+          <li className={`categories__link ${category === 'cardigans' && 'categories__link_active'}`} onClick={() => setCategory('cardigans')}>Cardigans</li>
+          <li className={`categories__link ${category === 'hoodies' && 'categories__link_active'}`} onClick={() => setCategory('hoodies')}>Hoodies</li>
         </ul>
         <div className="calculation-products">Showed 9 from 12 goods</div>
-          <ProductList/>
+        <ProductList category={category} />
         <div className="calculation-products">Showed 9 from 12 goods</div>
       </div>
     </div>
